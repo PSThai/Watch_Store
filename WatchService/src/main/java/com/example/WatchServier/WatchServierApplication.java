8915e9302bf1dc240e476a2d08a30dea44b81246
PSThai <93112500+PSThai@users.noreply.github.com>
2024-04-16 19:37:17 +0700

    config

---
+++ WatchService/src/main/java/com/example/WatchServier/WatchServierApplication.java
@@ -0,0 +1,13 @@
+package com.example.WatchServier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchServierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchServierApplication.class, args);
+	}
+
+}
